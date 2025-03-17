349d69676bec3646bc73b1304cdda3ce13832a91
Alya <149942623+Alya109@users.noreply.github.com>
2025-03-17 23:51:55 +0800

    Create Pokedex.py

---
+++ Pokedex.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def getSearchInput(search):
+
+def searchDatabase():
+
+def displayResult():
+
+def sysError(status):
+    match status:
+        case 404:
+            print("Error not found 404.")
